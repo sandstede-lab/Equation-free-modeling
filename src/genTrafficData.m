148832a19d95c71776eb17c0f0b78a5984ff2745
Rebecca Santorella <santorr2@tcnj.edu>
2021-02-11 10:29:01 -0500

    Updated generate data example

--- src/genTrafficData.m
+++ src/genTrafficData.m
@@ -1,4 +1,5 @@
 %% Generates data for traffic
+% genTrafficData(10000, 0.96, 1.1, 0.1, 3.5, 500, 10000, '../data/30data.mat')
 % dataPoints-   number of different simulations to run
 % v0Min-        min v0 to run each simulation
 % v0Max-        max v0 to run each simulation
@@ -11,14 +12,18 @@
 function genTrafficData(dataPoints, v0Min, v0Max, muMin, muMax,tMin, tMean, dataFile)
 
 options = odeset('AbsTol',10^-8,'RelTol',10^-8); % ODE 45 options
+rng(18); % set random seed
 h = 2.4;
 len = 60;
 numCars = 30;
 cars = zeros(numCars*2, 1);
-data = zeros(dataPoints, 2*numCars + 3);
+allData = zeros(dataPoints, 2*numCars + 3);
 
 for j=1:dataPoints
-    disp(j);
+    if mod(j, 100) ==0
+        disp(j);
+    end
+    
     % randomly generate initial condition, v0, t
     t= exprnd(tMean)+tMin; % drawn from shifted exponential
     v0 = (v0Max-v0Min)*rand()+v0Min;
@@ -31,17 +36,17 @@ for j=1:dataPoints
     end
 
     [~,allTime] = ode45(@microsystem,[0 t],cars, options, [v0 len h]);           % evolve cars
-    data(j, 1:2*numCars) = allTime(end,:);                                       % record the evolution
-    data(j, end) = v0;
-    data(j, end-1) = mu;
-    data(j, end-2) = t;
+    allData(j, 1:2*numCars) = allTime(end,:);                                       % record the evolution
+    allData(j, end) = v0;
+    allData(j, end-1) = mu;
+    allData(j, end-2) = t;
 
     if(mod(j,20)==0)
-        save(dataFile,'data');  % save the snapshots of the simulation
+        save(dataFile,'allData');  % save the snapshots of the simulation
     end
 end
 
-
-save(dataFile,'data');  % save the snapshots of the simulation
+hways = getHeadways(allData(:,1:numCars), len);
+save(dataFile,'allData', 'hways');  % save the snapshots of the simulation
 
 end
